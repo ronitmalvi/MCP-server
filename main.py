2e969af34cc486c7374a8405a154988fb634e43a
Ronit <malvironit1103@gmail.com>
2025-03-26 23:37:34 +0530

    initialising mcp server, helper functions

--- main.py
+++ main.py
@@ -1,3 +1,56 @@
+from mcp.server.fastmcp import FastMCP
+from dotenv import load_dotenv
+import httpx
+import os
+import json
+from bs4 import BeautifulSoup
+load_dotenv()
+
+mcp=FastMCP("docs")
+
+USER_AGENT="docs-app/1.0"
+SERPER_URL="https://google.serper.dev/search"
+
+docs_url={
+    "langchain":"python.langchain.com/docs",
+    "llama-index":"docs.llamaindex.ai/en/stable",
+    "openai":"platform.openai.com/docs",
+}
+
+async def search_web(query: str)->dict | None:
+    payload=json.dumps({"q":query,"num":2})
+
+    headers={
+        "X-API-KEY":os.getenv("SERPER_API_KEY"),
+        "Content-Type":"application/json"
+    }
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response=await client.post(SERPER_URL,data=payload,headers=headers,timeout=30)
+            response.raise_for_status()
+            return response.json()
+        except httpx.TimeoutException:
+            return {"organic":[]}
+    
+
+
+async def fetch_url(url:str):
+    async with httpx.AsyncClient() as client:
+        try:
+            response=await client.get(url,timeout=30)
+            soup=BeautifulSoup(response.text,"html.parser")
+            text=soup.get_text()
+            return text
+        except httpx.TimeoutException:
+            return "Timeout Error"
+
+@mcp.tool()
+def get_docs(query:str):
+    
+    
+
+
 def main():
     print("Hello from mcp-server!")
 
